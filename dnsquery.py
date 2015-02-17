b54b6f7f42ce7d71610ab3c5b27b847d3ce2b14b
pranavrajtyagi <pt992@nyu.edu>
2015-02-16 22:12:24 -0500

    DNS that uses dig command
    a python file that uses dig command

--- dnsquery.py
+++ dnsquery.py
@@ -5,19 +5,9 @@ link followed : https://docs.python.org/2/library/subprocess.html#module-subproc
 import subprocess
 import shlex
 
-NAME_SERVER_IP ="@8.8.8.8" #it is the server to be queried by default
 
-DNSlookup = 'dig NAME_SERVER_IP FQDN' #DNS Lookups for Fully qualified domain Name
+DNSlookup = 'dig FQDN' #DNS Lookups for Fully qualified domain Name
 FQDN = raw_input("enter the ip address you want to apply dig on : ")
-# cmd='dig @ns1.netnames.net www.rac.co.uk CNAME'
 process =subprocess.Popen(shlex.split(DNSlookup), stdout=subprocess.PIPE)
 out1,err=process.communicate()
 print(out1)
-
-'''
-DNSconfig = 'dig NAMESERVER FQDN'
-NAMESERVER = raw_input("enter the nameserver : ")
-process =subprocess.Popen(shlex.split(DNSconfig), stdout=subprocess.PIPE)
-out2,err=process.communicate()
-print(out2)
-'''
