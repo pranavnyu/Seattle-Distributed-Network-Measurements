f0e44ec302fc5192130f7547e2a3438ac1c646ef
pranavrajtyagi <pt992@nyu.edu>
2015-02-19 01:26:58 -0500

    Python Server/Client connection
    Simple Server/Client connection

---
+++ simple socket programs/client.py
@@ -0,0 +1,12 @@
+# Echo client program
+import socket
+
+HOST = ''    # The remote host
+PORT = 8000              # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.send('Hello, world')
+data = s.recv(1024)
+s.close()
+print 'Received', repr(data)
+
