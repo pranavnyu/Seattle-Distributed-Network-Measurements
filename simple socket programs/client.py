1fe93f92a86878b11c82ed9a7c541583c645b27c
pranavrajtyagi <pt992@nyu.edu>
2015-02-26 21:41:33 -0500

    simple client file
    simple socket programming to send custom message to server

--- simple socket programs/client.py
+++ simple socket programs/client.py
@@ -5,7 +5,8 @@ HOST = ''    # The remote host
 PORT = 8000              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.send('Hello, world')
+message = raw_input("enter the message here")
+s.send(message)
 data = s.recv(1024)
 s.close()
 print 'Received', repr(data)
